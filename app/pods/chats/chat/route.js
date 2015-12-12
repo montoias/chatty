f34e36cafca6b5fd7f60594f08e5c242633fa0b2
Paulo Monteiro <paulomonteiro7.pm@gmail.com>
2015-12-12 02:14:03 +0000

    feat(message): Add message structure

--- app/pods/chats/chat/route.js
+++ app/pods/chats/chat/route.js
@@ -1,27 +1,35 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import moment from 'npm:moment';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: Ember.inject.service(),
 
-  model(){
-    return Ember.RSVP.hash({
-      messages: this.store.findAll('message'),
-      actions: {
-        sendMessage: this.actions.sendMessage.bind(this),
-      }
-    });
+  model({chat_id}){
+    return this.store.find('conversation', chat_id);
   },
 
   actions: {
     sendMessage(msg) {
-      let message = this.store.createRecord('message', {
-        message: msg
-      });
-
+      // clean input box
       this.set('currentModel.message', '');
 
-      return message.save();
+      let user = this.store.find('user', this.get('session').session.content.authenticated.uid);
+      let conversation = this.get('currentModel');
+      let message = this.store.createRecord('message', {
+        body: msg,
+        timestamp: moment()
+      });
+
+      conversation.get('messages').pushObject(message);
+
+      return user.then((user) => {
+        message.set('user', user);
+
+        message.save().then(() => {
+          conversation.save();
+        });
+      })
     }
   }
 });
--- app/pods/message/model.js
+++ app/pods/message/model.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  message: DS.attr('string')
+  body: DS.attr('string'),
+  timestamp: DS.attr('string'),
+  user: DS.belongsTo('user')
 });
